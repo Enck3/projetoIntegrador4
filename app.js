575ee9ef46b52cf99fe5e3b2067ddfe2f7c47774
Lucas <lucas06022004@hotmail.com>
2024-10-28 20:42:31 -0300

    cadastro usuário e login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { MongoClient } from 'mongodb';
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bcrypt = require('bcrypt')
+
 
 
 const { MongoClient } = require('mongodb');
@@ -20,6 +22,8 @@ const connectDB = async () => {
         process.exit(1); 
     }
 };
+
+
 
 
 connectDB();
@@ -70,6 +74,88 @@ app.post('/user/register', async (req, res) => {
         res.status(500).json({ message: 'Erro ao cadastrar usuário' });
     }
 });
+
+
+app.post('/user/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Verificar se os campos foram fornecidos
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Por favor, forneça email e password' });
+        }
+
+        // Acessar a coleção de usuários
+        const db = client.db('projetoIntegrador'); // Substitua pelo nome do seu banco
+        const usersCollection = db.collection('users');
+
+        // Verificar se o usuário ou e-mail já existe
+        const user = await usersCollection.findOne( { email: email } );
+        if (!user) {
+            return res.status(400).json({ message: 'Usuário não existente' });
+        }
+
+        // Criptografar a senha antes de armazená-la
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(400).json({ message: 'Senha incorreta.' });
+        }
+
+       
+
+        
+        
+
+        
+        
+
+        res.status(201).json({ message: 'Login realizado com sucesso', userId: user._id });
+    } catch (error) {
+        console.error('Erro ao cadastrar usuário:', error);
+        res.status(500).json({ message: 'Erro ao cadastrar usuário' });
+    }
+});
+
+
+//aqui vai a rota de cadastro de macenicos
+
+
+app.post('/mecanico/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Verificar se os campos foram fornecidos
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Por favor, forneça email e password' });
+        }
+
+        // Acessar a coleção de usuários
+        const db = client.db('projetoIntegrador'); // Substitua pelo nome do seu banco
+        const usersCollection = db.collection('mecanicos');
+
+        // Verificar se o usuário ou e-mail já existe
+        const user = await usersCollection.findOne( { email: email } );
+        if (!user) {
+            return res.status(400).json({ message: 'Usuário não existente' });
+        }
+
+        // Criptografar a senha antes de armazená-la
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(400).json({ message: 'Senha incorreta.' });
+        }
+
+
+        res.status(201).json({ message: 'Login realizado com sucesso', userId: user._id });
+    } catch (error) {
+        console.error('Erro ao cadastrar usuário:', error);
+        res.status(500).json({ message: 'Erro ao cadastrar usuário' });
+    }
+});
+
+
+
+
 
 // Iniciar o servidor
 app.listen(PORT, () => {
